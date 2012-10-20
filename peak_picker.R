e89dffc7e3dd0627851c95933e2306ce3c8ac66c
Kevin Murray <k.d.murray.91@gmail.com>
2012-10-21 01:16:11 +1100

    peak_pick appears to work, not sure if the algorithm is the best

--- peak_picker.R
+++ peak_picker.R
@@ -1,17 +1,6 @@
 # this file should be source()-able to import this function
 library(caTools)
 
-# data = c(1,32,33,4,3,4,3,42,2,3,4,34,34344,2,3,4,32,4,3,4,4,4,43,343,2,3,8,98,8,7,3)
-# plot (data)#,ylim=c(-50000,50000))
-# lines(1:length(data), kmean)
-# lines(1:length(data), upper)
-# lines(1:length(data), lower)
-# points(data_nopeaks, pch="+")
-# data <- data_nopeaks
-# data[data == max(data)] = NA 
-# plot(1:length(data),km.runsd(data))
-# plot(1:length(data),runsd(data,5))
-
 km.runsd <- function(data){
   data.runsd <- numeric(length=length(data))
   for (i in 1:length(data)){
@@ -22,6 +11,7 @@ km.runsd <- function(data){
   }
   return(data.runsd)
 }
+
 peak.pick <- function(data){
   kmean <- runmean(data,15)
   ksd = 1 * sd(data)
@@ -30,6 +20,7 @@ peak.pick <- function(data){
   peaks <- (upper < data) | (data < lower)
   return(peaks)
 }
+
 peak.convert <- function (data){
   peaks <- peak.pick(data)  
   data_nopeaks <- data
@@ -38,16 +29,10 @@ peak.convert <- function (data){
   data_nopeaks[peaks] = kmean[peaks]
   return(data_nopeaks)
 }
+
 peaks.only <- function (data){
   peaks <- peak.pick(data)
   data_peaks <- data
   data_peaks[!peaks] = NA
   return(data_peaks)
 }
-
-# plot(1:length(data), data)
-# peak.pick(data)
-# plot(1:length(data), peak.convert(data))
-# peak.convert(data)
-# plot(1:length(data), peaks.only(data))
-# peaks.only(data)
