556a908d69ba7a3601d6539bd1a8a21949c0600b
Kevin Murray <k.d.murray.91@gmail.com>
2012-10-23 19:28:44 +1100

    finished paresnip_summarise.R, i think.

--- paresnip_summarise.R
+++ paresnip_summarise.R
@@ -1,5 +1,18 @@
 args <- commandArgs(trailingOnly=T)
 args[1] = "alx8_conserved.tab"
-paresnip.replicated <- read.delim(args[1], header=T, sep='\t', quote="\"")
+args[2] = "alx8"
 
+paresnip.filtered <- read.delim(args[1], header=T, sep='\t', quote="\"")
+SRA.FA.cols <- grep("^SRA|FA",names(paresnip.filtered))
+paresnip.filtered <- paresnip.filtered[,c(1,2,3,SRA.FA.cols)]
 
+paresnip.summarised <- paresnip.filtered
+SRA.cols <- grep("^SRA",names(paresnip.summarised))
+FA.cols <- grep("^FA",names(paresnip.summarised))
+paresnip.summarised$SRA.mean <- apply(paresnip.summarised[,SRA.cols], 1, mean)
+paresnip.summarised$FA.mean <- apply(paresnip.summarised[,FA.cols], 1, mean)
+
+paresnip.summarised$SRA.sd <- apply(paresnip.summarised[,SRA.cols], 1, sd)
+paresnip.summarised$FA.sd <- apply(paresnip.summarised[,FA.cols], 1, sd)
+
+write.csv(paresnip.summarised, paste(args[2], "summarised.csv", sep="_"))
