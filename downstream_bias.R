2301d3dc2de024ec5656f65aa3483e1eb04b3d1e
Kevin Murray <k.d.murray.91@gmail.com>
2012-10-24 17:55:23 +1100

    slight refactoring of downstream_bias.R

--- downstream_bias.R
+++ downstream_bias.R
@@ -5,6 +5,8 @@ library(iterators)
 
 args <- commandArgs(trailingOnly=T)
 
+## READ DATA
+
 agi.peaks <- read.csv(args[1])
 #[,1]: agi
 #[,2]: peak pos or NA
@@ -19,20 +21,28 @@ RNAseq.filtered <- read.csv(args[3])
 #[,3]: mapq, mapping qualtity score
 #[,4]: cigar, mapping indel string
 
+
+## CALCULATE BIAS TABLE
+
 # pre-fill summarised table based on all agis
-RNAseq.summarised <- data.frame(
+RNAseq.cleavage.bias <- data.frame(
     agi=agi.lengths$agi,
     upstream=numeric(length(agi.lengths$agi)), 
     downstream=numeric(length(agi.lengths$agi))
     )
 
+# for every agi in the rnaseq bam, find the number of reads upstream and 
+# downstream of the cleavage site, then find RPK of these
 RNAseq.iter <- isplit(deg, deg$rname)
-
 foreach (RNAseq.agi=RNAseq.iter) %dopar%{
+    
     agi <- RNAseq.agi$key[[1]]
+    agi.length <- agi.lengths[agi.lengths[,1]==agi, 2]
+    
+    # get a peak position, if there's no peak, aim at half of the length
     peak.position <- agi.peaks[agi.peaks[,1]==agi, 2] #[,1] = agi, [,2]=pos or NA
     if (is.na(peak.position)){
-        peak.position <- as.integer(agi.lengths[agi.lengths[,1]==agi, 2] / 2)
+        peak.position <- as.integer(agi.length/ 2)
     }
     if (is.na(peak.position)){
         next
@@ -42,9 +52,13 @@ foreach (RNAseq.agi=RNAseq.iter) %dopar%{
     upstream.positions <- length(positions[positions<peak.position])
     downstream.positions <- length(positions[positions>=peak.position])
     
-    RNAseq.summarised[RNAseq.summarised[,1]==agi,] <- c(
+    RNAseq.cleavage.bias[RNAseq.cleavage.bias[,1]==agi,] <- c(
         agi,
         upstream.positions,
         downstream.positions
         )
 }
+
+write.csv(RNAseq.cleavage.bias,paste(args[4], "cleavage_bias", sep="_"))
+
+## DO TESTS
