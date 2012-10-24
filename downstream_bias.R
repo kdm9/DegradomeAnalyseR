5b7ae6b552c3a951cf906117f7e0df69f262b058
Kevin Murray <k.d.murray.91@gmail.com>
2012-10-24 16:46:51 +1100

    added downstream_bias.R, not tested

---
+++ downstream_bias.R
@@ -0,0 +1,50 @@
+library(doMC)
+# registerDoMC(cores=2)
+library(foreach)
+library(iterators)
+
+args <- commandArgs(trailingOnly=T)
+
+agi.peaks <- read.csv(args[1])
+#[,1]: agi
+#[,2]: peak pos or NA
+
+agi.lengths <- read.csv(args[2])
+#[,1]: agi
+#[,2]: agi length
+
+RNAseq.filtered <- read.csv(args[3])
+#[,1]: rname, which is an agi
+#[,2]: pos, mapping position of read
+#[,3]: mapq, mapping qualtity score
+#[,4]: cigar, mapping indel string
+
+# pre-fill summarised table based on all agis
+RNAseq.summarised <- data.frame(
+    agi=agi.lengths$agi,
+    upstream=numeric(length(agi.lengths$agi)), 
+    downstream=numeric(length(agi.lengths$agi))
+    )
+
+RNAseq.iter <- isplit(deg, deg$rname)
+
+foreach (RNAseq.agi=RNAseq.iter) %dopar%{
+    agi <- RNAseq.agi$key[[1]]
+    peak.position <- agi.peaks[agi.peaks[,1]==agi, 2] #[,1] = agi, [,2]=pos or NA
+    if (is.na(peak.position)){
+        peak.position <- as.integer(agi.lengths[agi.lengths[,1]==agi, 2] / 2)
+    }
+    if (is.na(peak.position)){
+        next
+    }
+    
+    positions <- as.numeric(RNAseq.agi$value$pos)
+    upstream.positions <- length(positions[positions<peak.position])
+    downstream.positions <- length(positions[positions>=peak.position])
+    
+    RNAseq.summarised[RNAseq.summarised[,1]==agi,] <- c(
+        agi,
+        upstream.positions,
+        downstream.positions
+        )
+}
