ba99306910968a8ffe63a99f147b7878ab76ff12
Kevin Murray <k.d.murray.91@gmail.com>
2012-10-24 16:45:22 +1100

    remove debug lines and fix long lines in plot_overall.R

--- plot_overall.R
+++ plot_overall.R
@@ -14,14 +14,18 @@ targets <- read.csv(args[2])
 
 agis <- isplit(deg, deg$rname)
 
-overall.hist <- data.frame(rname=character(), pos=numeric(), count=numeric(), m_mapq=numeric())
-agi = nextElem(agis)
+overall.hist <- data.frame(
+    rname=character(),
+    pos=numeric(),
+    count=numeric(),
+    m_mapq=numeric()
+    )
+
 foreach(agi=agis) %dopar% {
-  agi = nextElem(agis)
-	seq.len = targets[targets[,1] == agi$key[[1]], 2]
-	agi$value$pos <- agi$value$pos / seq.len
-	overall.hist <- merge(overall.hist, data.frame(agi$value)[,2:5],all=T)
-	write.csv(overall.hist, file=paste(args[3],"_normalised.csv", sep=""))
+    seq.len = targets[targets[,1] == agi$key[[1]], 2]
+    agi$value$pos <- agi$value$pos / seq.len
+    overall.hist <- merge(overall.hist, data.frame(agi$value)[,2:5],all=T)
+    write.csv(overall.hist, file=paste(args[3],"_normalised.csv", sep=""))
 }
 pdf(paste(args[3],"_overall.pdf", sep=""))
 plot(overall.hist$pos, overall.hist$count)
