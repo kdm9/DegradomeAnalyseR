b231c24b23cc092495ff8dd5688d28baf3f9cba6
Kevin Murray <k.d.murray.91@gmail.com>
2012-10-31 17:36:45 +1100

    added header to sanitise_paresnip_output.py

--- sanitise_paresnip_output.py
+++ sanitise_paresnip_output.py
@@ -2,6 +2,7 @@ import re
 from sys import argv
 fh = open(argv[1])
 this_peak = []
+print("Gene\tCategory\tCleavage Position\tP-Value\tFragment Abundance\tWeighted Fragment Abundance\tNormalised Weighted Fragment Abundance\tAlignment Score\tShort Read ID\tShort Read Abundance\tNormalised Short Read Abundance\tDuplex")
 for line in fh:
     if line != "-----\n":
         line = line.strip()
