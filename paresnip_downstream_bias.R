26145d7984c8c9ec15668c988461def4801429ea
Kevin Murray <k.d.murray.91@gmail.com>
2012-10-31 15:56:06 +1100

    paresnip_downstream_bias.R finished but for tests

--- paresnip_downstream_bias.R
+++ paresnip_downstream_bias.R
@@ -4,9 +4,12 @@ library(foreach)
 library(iterators)
 
 args <- commandArgs(trailingOnly=T)
+# args[1] = "alx8_summarised.csv"
+# args[2] = "header.csv"
+# args[3] = "alx8_rnaseq.filtered.csv"
+# args[4] = "alx8"
 
-## READ DATA
-
+### READ DATA
 agi.peaks <- read.csv(args[1])
 #[,1]: agi
 #[,2]: peak pos or NA
@@ -24,15 +27,20 @@ RNAseq.filtered <- read.csv(args[3])
 #[,4]: cigar, mapping indel string
 
 
-## CALCULATE BIAS TABLE
+### CALCULATE BIAS TABLE
 # pre-fill summarised table based on all agis
-RNAseq.cleavage.bias <- data.frame(
+RNAseq.cleavage.summary <- data.frame(
     agi=agi.lengths$agi,
-    upstream.positions=numeric(length(agi.lengths$agi)),,
-    downstream.positions=numeric(length(agi.lengths$agi)),,
-    upstream.length=numeric(length(agi.lengths$agi)),,
+    upstream.count=numeric(length(agi.lengths$agi)),
+    downstream.count=numeric(length(agi.lengths$agi)),
+    upstream.length=numeric(length(agi.lengths$agi)),
     downstream.length=numeric(length(agi.lengths$agi)),
     )
+
+RNAseq.cleavage.bias <- data.frame(
+  agi=agi.lengths$agi,
+  bias=numeric(length(agi.lengths$agi)),
+)
 
 # for every agi in the rnaseq bam, find the number of reads upstream and 
 # downstream of the cleavage site, then find RPK of these
@@ -58,17 +66,31 @@ foreach (RNAseq.agi=RNAseq.iter) %dopar%{
     upstream.count <- length(positions[positions<peak.position])
     downstream.count <- length(positions[positions>=peak.position])
     
-    # Fill RNAseq cleavage bias table
-    RNAseq.cleavage.bias[RNAseq.cleavage.bias[,1]==agi,] <- c(
+    # Fill RNAseq cleavage summary table
+    RNAseq.cleavage.summary[RNAseq.cleavage.summary[,1]==agi,] <- c(
         agi,
-        upstream.positions,
-        downstream.positions,
+        upstream.count,
+        downstream.count,
         upstream.length,
         downstream.length
         )
+    
+    # calculate bias
+    upstream.RPK <- upstream.count / (upstream.length / 1000)
+    downstream.RPK <- downstream.count / (downstream.length / 1000)
+    
+    bias <- downstream.RPK / upstream.RPK
+    
+    # Fill RNAseq cleavage bias table
+    RNAseq.cleavage.bias[RNAseq.cleavage.bias[,1]==agi,] <- c(
+      agi,
+      bias
+    )
+    
+    
 }
 
-write.csv(RNAseq.cleavage.bias,paste(args[4], "cleavage_bias", sep="_"))
+write.csv(RNAseq.cleavage.summary,file=paste(args[4], "cleavage.bias.csv", sep="."))
+write.csv(RNAseq.cleavage.bias,file=paste(args[4], "cleavage.bias.csv", sep="."))
 
 ## DO TESTS
-
